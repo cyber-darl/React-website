dd3215a56a3bc7b118d218d1455ccd41999550c0
cyber-darl <onosdarlington939@gmail.com>
2024-06-29 04:18:06 -0400

    yeah

--- src/App.js
+++ src/App.js
@@ -4,14 +4,16 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Navbar from './Components/Navbar/Navbar';
 import './App.css';
+import Landing_Page from './Components/Landing_Page/Landing_Page'
 
 function App() {
 
   return (
     <div className="App">
         <BrowserRouter>
-          <Navbar/>
+        
               <Routes>
+              <Route path="/" element={<Landing_Page/>}/>
               </Routes>
             
         </BrowserRouter>
